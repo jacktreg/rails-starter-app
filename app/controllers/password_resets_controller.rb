5ffac3fd6954bb48185be5749bff6d158a534bd8
Jack Tregurtha <jacktregurtha@gmail.com>
2018-06-18 16:19:36 -0400

    Updated form views using bootstrap and generated password resets controller

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,14 @@
+class PasswordResetsController < ApplicationController
+  
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-
   root 'application#home'
 
   get '/about',      to: 'application#about'
@@ -13,5 +11,6 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resources :users, param: :username
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
